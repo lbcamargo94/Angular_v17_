bac7f6ee5eff9c2eff1caaa6fb606050a1fbdca5
HTM - Lucas Camargo <122825577+HTM-Lucas@users.noreply.github.com>
2024-01-25 10:58:59 -0300

    feat: section 7.35
    @Switch Case complete

--- src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,17 +1,51 @@
-import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  AsyncPipe,
+  CommonModule,
+  NgFor,
+  NgIf,
+  NgSwitchCase,
+  NgSwitchDefault,
+} from '@angular/common';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule, NgIf],
+  imports: [
+    CommonModule,
+    NgIf,
+    NgFor,
+    AsyncPipe,
+    NgSwitchCase,
+    NgSwitchDefault,
+  ],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss',
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
 
+  public itens = [{ name: 'Luiz Swagger' }];
+
+  public switchCondition = 'A';
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+  ]).pipe(delay(3000));
+
   public handleView() {
     this.isTrue = !this.isTrue;
+  }
+
+  public trackByFn(index: number) {
+    return index;
+  }
+
+  public addNewName(value: string) {
+    return this.itens.push({ name: value });
   }
 }
